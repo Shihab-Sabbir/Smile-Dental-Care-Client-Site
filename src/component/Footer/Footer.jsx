9879fb24406e04a429d6111d62bfaead02e22f43
shihab-sabbir <shihab11231@gmail.com>
2022-11-09 16:02:14 +0600

    footer added

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import logo from '../../asset/logo.png'
 function Footer() {
     return (
         <div>
             <footer className="p-4 bg-slate-100 sm:p-6 dark:bg-gray-900">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" className="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" alt="FlowBite Logo" />
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                        </a>
+                        <Link to='/' className="flex items-center">
+                            <img src={logo} className="mr-3 h-8" alt="Logo" />
+                        </Link>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
@@ -27,10 +27,10 @@ function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                             <ul className="text-gray-600 dark:text-gray-400">
                                 <li className="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                    <a href="https://github.com/Shihab-Sabbir" target="_blank" className="hover:underline ">Github</a>
                                 </li>
                                 <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                    <a href="https://github.com/Shihab-Sabbir" target="_blank" className="hover:underline">Discord</a>
                                 </li>
                             </ul>
                         </div>
@@ -49,7 +49,7 @@ function Footer() {
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/Shihab-Sabbir" target="_blank" className="hover:underline">Smile™</a>. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                         <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
