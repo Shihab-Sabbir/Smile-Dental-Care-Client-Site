942a23e6aff7ea7fe3bf3cfe5d67a269ab06b4d2
shihab-sabbir <shihab11231@gmail.com>
2022-11-10 19:04:24 +0600

    loader added

--- src/component/ServiceDetails/ServiceDetails.jsx
+++ src/component/ServiceDetails/ServiceDetails.jsx
@@ -7,13 +7,33 @@ import ReviewForm from "../ReviewForm/ReviewForm";
 import { Helmet } from "react-helmet";
 const ServiceDetails = () => {
     const [reviews, setReviews] = useState([])
+    const [dataLoading, setDataLoading] = useState(true);
     const service = useLoaderData();
     const { user, updateState } = useContext(AuthContext);
-    const { title, price, clinicName, description1, serviceId, description2, descriptionHeader, displayImage, InsideImage, treatmentTime } = service;
+    const { title, price, description1, serviceId, description2, descriptionHeader, displayImage, InsideImage, treatmentTime } = service;
     const location = useLocation();
     useEffect(() => {
-        fetch(` https://assignment-11-five.vercel.app/review/${serviceId}`).then(res => res.json()).then(data => setReviews(data)).catch(err => console.log(err))
+        fetch(` https://assignment-11-five.vercel.app/review/${serviceId}`).then(res => res.json()).then(data => { setReviews(data); setDataLoading(false) }).catch(err => console.log(err))
     }, [updateState])
+
+    if (dataLoading) {
+        return (
+            <div className=" rounded relative mt-[100px]">
+                <div className="rounded-full bg-[#b8ebf0] w-[190px] h-[190px] relative flex justify-center items-center mx-auto animate-spin">
+                    <svg className="absolute top-[2px] right-0" width={76} height={97} viewBox="0 0 76 97" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <mask id="path-1-inside-1_2495_2146" fill="white">
+                            <path d="M76 97C76 75.6829 69.2552 54.9123 56.7313 37.6621C44.2074 20.4118 26.5466 7.56643 6.27743 0.964994L0.0860505 19.9752C16.343 25.2698 30.5078 35.5725 40.5526 49.408C50.5974 63.2436 56.007 79.9026 56.007 97H76Z" />
+                        </mask>
+                        <path d="M76 97C76 75.6829 69.2552 54.9123 56.7313 37.6621C44.2074 20.4118 26.5466 7.56643 6.27743 0.964994L0.0860505 19.9752C16.343 25.2698 30.5078 35.5725 40.5526 49.408C50.5974 63.2436 56.007 79.9026 56.007 97H76Z" stroke="#1ab7c5" strokeWidth={40} mask="url(#path-1-inside-1_2495_2146)" />
+                    </svg>
+                    <div className="rounded-full bg-white w-[150px] h-[150px]" />
+                </div>
+                <p className="absolute mx-auto inset-x-0 my-auto inset-y-[80px] text-base font-medium text-gray-800 text-center">
+                    Loading ...
+                </p>
+            </div>
+        )
+    }
     return (
         <div>
             <Helmet>
