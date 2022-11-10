3711c94aa078b9da25e81b9db86e052b30c3309f
shihab-sabbir <shihab11231@gmail.com>
2022-11-10 13:43:10 +0600

    over all appearance update

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -26,11 +26,8 @@ function Header() {
                             alt="Flowbite Logo"
                         />
                     </Link>
-                    <span className="self-center whitespace-nowrap text-xl font-[PT Sans Narrow] text-[#00ACBD]">
-
-                    </span>
                 </Navbar.Brand>
-                <div className="flex gap-2 xl:gap-4 items-center md:order-2">
+                <div className="flex gap-1 xl:gap-4 items-center md:order-2 lg:mt-0 mt-0 md:mt-2">
                     {user?.uid != null && <Dropdown
                         arrowIcon={false}
                         inline={true}
@@ -60,7 +57,7 @@ function Header() {
                     </div>
                     <Navbar.Toggle />
                 </div>
-                <Navbar.Collapse style={{ alignItems: 'center' }} className='x'>
+                <Navbar.Collapse className='x'>
                     <NavLink
                         to="/"
                         end
@@ -103,8 +100,8 @@ function Header() {
                     >
                         {user?.uid ? "Logout" : "Login"}
                     </NavLink>
-                    <div>
-                        <form action='' className=" flex justify-start items-center relative" onSubmit={(e) => { handleSearch(e); navigate('/search'); }}>
+                    <div className='p-2 max-w-[500px] mx-auto hidden lg:block'>
+                        <form action='' className="flex justify-start items-center relative" onSubmit={(e) => { handleSearch(e); navigate('/search'); }}>
                             <input type="text" name="search" id="" className="text-sm leading-none text-left text-gray-600 px-4 py-3 w-full border rounded border-gray-300  outline-none" placeholder='Search Service...' required />
                             <button type='submit' className='flex justify-center items-center'>
                                 <svg
@@ -134,6 +131,71 @@ function Header() {
                             </button>
                         </form>
                     </div>
+                    <div className='lg:hidden'>
+                        <label htmlFor="my-modal" >
+                            <svg
+
+                                className="z-10 cursor-pointer lg:ml-[100px]"
+                                width={24}
+                                height={24}
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+                                    stroke="white"
+                                    strokeWidth="2.66667"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                                <path
+                                    d="M21 21L15 15"
+                                    stroke="white"
+                                    strokeWidth="2.66667"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </label>
+                        <input type="checkbox" id="my-modal" className="modal-toggle" />
+                        <div className="modal">
+                            <div className="modal-box">
+                                <div>
+                                    <form action='' className=" flex justify-start items-center relative" onSubmit={(e) => { handleSearch(e); navigate('/search'); }}>
+                                        <input type="text" name="search" id="" className="text-sm leading-none text-left text-gray-600 px-4 py-3 w-full border rounded border-gray-300  outline-none" placeholder='Search Service...' required />
+                                        <button type='submit' className='flex justify-center items-center'>
+                                            <svg
+
+                                                className="absolute right-3 z-10 cursor-pointer"
+                                                width={24}
+                                                height={24}
+                                                viewBox="0 0 24 24"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+                                                    stroke="#4B5563"
+                                                    strokeWidth="1.66667"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                />
+                                                <path
+                                                    d="M21 21L15 15"
+                                                    stroke="#4B5563"
+                                                    strokeWidth="1.66667"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                                <label htmlFor="my-modal" className="btn mt-5">Cancle</label>
+                            </div>
+                        </div>
+                    </div>
                     <Navbar.Link>
                         <div className='absolute top-[69px] right-[12px] sm:hidden border-2 p-1 rounded-md'>
                             {dark ? <MdOutlineDarkMode className='text-xl cursor-pointer' title='click to light mode' onClick={handleDark} /> : <MdOutlineLightMode className='text-xl cursor-pointer' title='click to light mode' onClick={handleDark} />}
@@ -141,6 +203,7 @@ function Header() {
                     </Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
+
         </div>
     )
 }
